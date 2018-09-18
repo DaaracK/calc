bcb23f5064656d9335ab0b889827158fa19612b1
vinicius kovatch de mattos <vinikovatch123@hotmail.com>
2018-09-17 21:57:50 -0300

    adicao de novos metodos

--- Calculadora.java
+++ Calculadora.java
@@ -6,4 +6,26 @@ public class Calculadora {
     public int dividir(int num, int den) {
 	return num/den;
     }
+    public int mul(int x,int y){
+        return x * y;
+    }
+    public int dif(int x,int y){
+        return x - y;
+    }
+    public int max(int x,int y){
+        if(x > y){
+        return x;
+    }
+        else{
+        return y;
+    }
+    }
+    public int min(int x,int y){
+        if(x < y){
+        return x;
+    }
+        else{
+        return y;
+    }
+    }
 }
